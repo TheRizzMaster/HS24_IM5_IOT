a6b2cb581ee0602fc125daed1c24e9ecc2ea6544
siro rudolf <siro.rudolf@stud.fhgr.ch>
2024-10-31 07:59:58 +0100

    Update tables.sql

--- web/backend/data/tables.sql
+++ web/backend/data/tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
     id CHAR(36) PRIMARY KEY,  -- UUID
-    card_id VARCHAR(255) UNIQUE NOT NULL
+    card_id VARCHAR(255) UNIQUE NOT NULL,
     firstname VARCHAR(255),
-    lastname VARCHAR(255),
+    lastname VARCHAR(255)
 );
 
 CREATE TABLE work_sessions (
