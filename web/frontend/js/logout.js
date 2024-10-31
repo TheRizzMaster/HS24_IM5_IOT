bcebb2f6017ab0507b2eff9350803ed94705256d
siro rudolf <siro.rudolf@stud.fhgr.ch>
2024-10-31 07:46:31 +0100

    Update logout.js

--- web/frontend/js/logout.js
+++ web/frontend/js/logout.js
@@ -8,7 +8,7 @@ function logout() {
     .then(data => {
         console.log(data.message); // e.g., 'Logout successful'
         // Redirect to login page
-        window.location.href = 'login.html';
+        window.location.href = 'index.html';
     })
     .catch(error => console.error('Error:', error));
 }
